eb46a0ca8bc3b93732ec496a17d16f29badd49be
Alejo9601 <alejandro96jm@gmail.com>
2022-04-25 10:04:55 -0300

    Added name to the character, refactored some styles and logic

--- src/components/CharacterImage.js
+++ src/components/CharacterImage.js
@@ -2,31 +2,37 @@ import styled from "styled-components";
 
 const ImgContainer = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   margin: 20px;
   /* background-color: gray; */
 `;
+const CharName = styled.h1`
+  font-size: 4rem;
+  padding-bottom: 20px;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+`;
 const Img = styled.img`
-  height: 90%;
-  width: 90%;
+  height: 80%;
+  width: 80%;
   object-fit: cover;
   box-shadow: -5px 5px 10px 0px black;
 `;
+
 const CharacterImage = ({ charData }) => {
   const imgSizes = ["/detail", "/portrait_uncanny", ""];
+  const imageUrl = charData.data.results[0].thumbnail.path.replace(
+    "http",
+    "https"
+  );
+  const charName = charData.data.results[0].name;
 
   return (
     <ImgContainer>
+      <CharName>{charName}</CharName>
       <Img
-        src={
-          charData
-            ? `${charData.data.results[0].thumbnail.path.replace(
-                "http",
-                "https"
-              )}${imgSizes[2]}.jpg`
-            : ""
-        }
+        src={charData ? `${imageUrl}${imgSizes[2]}.jpg` : ""}
         alt="character"
       />
     </ImgContainer>
