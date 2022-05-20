7e3d5a8f5b1cb93b2ad9325c51088f1d6b34aa84
Alejo9601 <alejandro96jm@gmail.com>
2022-05-20 12:50:06 -0300

    Deleted rootMargin added threshold on Visor component

--- src/components/Visor.js
+++ src/components/Visor.js
@@ -4,16 +4,17 @@ import Loader from "./Loader";
 import styled from "styled-components";
 
 const TopBottomVisor = styled.div`
+  height: 20px;
   width: 100%;
   text-align: center;
-  background-color: blue;
+  background-color: transparent;
   position: relative;
 `;
 
 const Visor = ({ toDoWhenReached }) => {
   const toObserve = useRef();
   const [observer, setElements, entries] = useObserver({
-    rootMargin: "500px",
+    threshold: 1,
     root: null,
   });
 
@@ -33,7 +34,6 @@ const Visor = ({ toDoWhenReached }) => {
   return (
     <TopBottomVisor ref={toObserve}>
       <Loader />
-      <h1>VISOR</h1>
     </TopBottomVisor>
   );
 };
