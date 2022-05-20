43589c7af9ca54bb3ab2806e0fe8eb8ac2228354
Alejo9601 <alejandro96jm@gmail.com>
2022-05-20 00:07:21 -0300

    Renamed function implemented camel case

--- src/helpers/TrimText.js
+++ src/helpers/TrimText.js
@@ -1,8 +1,6 @@
-const TrimText = (p, max_chars) => {
+export const trimText = (p, max_chars) => {
   if (p.length > max_chars) {
     p = p.substr(0, max_chars) + " ...";
   }
   return p;
 };
-
-export default TrimText;
